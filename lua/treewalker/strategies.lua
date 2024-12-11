7c28e43520019422eaf6a5b21397d93b5cf6ca0c
Aaron Sullivan <aasullivan1618@gmail.com>
2024-12-10 19:51:22 -0800

    Fix weird ish where up/down over end was diff

--- lua/treewalker/strategies.lua
+++ lua/treewalker/strategies.lua
@@ -16,10 +16,9 @@ local function get_next_jump_candidate(current_lnum, dir)
   end
   local max_lnum = vim.api.nvim_buf_line_count(0)
   if candidate_lnum > max_lnum or candidate_lnum <= 0 then return end
-  local current_line = lines.get_line(current_lnum)
   local candidate_line = lines.get_line(candidate_lnum)
-  local current_indentation = lines.get_start_col(current_line)
-  local candidate = vim.treesitter.get_node({ pos = { candidate_lnum - 1, current_indentation } })
+  local candidate_col = lines.get_start_col(candidate_line)
+  local candidate = vim.treesitter.get_node({ pos = { candidate_lnum - 1, candidate_col } })
   return candidate_lnum, candidate_line, candidate
 end
 
@@ -28,19 +27,19 @@ local M = {}
 -- Skip unwanted nodes
 -- TODO this should not jump to other functions
 ---@param dir Dir
----@param lnum integer
----@param indent integer
+---@param row integer
+---@param col integer
 ---@return integer | nil, string | nil, TSNode | nil
-function M.get_next_vertical_target_at_same_col(dir, lnum, indent)
-  local candidate_lnum, candidate_line, candidate = get_next_jump_candidate(lnum, dir)
+function M.get_next_vertical_target_at_same_col(dir, row, col)
+  local candidate_lnum, candidate_line, candidate = get_next_jump_candidate(row, dir)
 
   while candidate_lnum and candidate_line and candidate do
-    local candidate_indent = lines.get_start_col(candidate_line)
+    local candidate_col = lines.get_start_col(candidate_line)
     local srow, scol, erow, ecol = candidate:range()
     if
         nodes.is_jump_target(candidate) -- only node types we consider jump targets
         and candidate_line ~= "" -- no empty lines
-        and candidate_indent == indent -- stay at current indent level
+        and candidate_col == col -- stay at current indent level
         -- and candidate_lnum == srow + 1 -- top of block; no end's or else's etc.
     then
       break -- use most recent assignment below
