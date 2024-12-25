1bad89f29ea91d60e5020065544249248bd815c2
Aleksey Imuzov <github@aimuzov.online>
2024-12-25 22:11:57 +0400

    Fix indents

--- lua/treewalker/init.lua
+++ lua/treewalker/init.lua
@@ -9,28 +9,28 @@ local Treewalker = {}
 -- Default setup() options
 ---@type Opts
 Treewalker.opts = {
-	highlight = true,
-	highlight_duration = 250,
-	highlight_group = "ColorColumn",
+  highlight = true,
+  highlight_duration = 250,
+  highlight_group = "ColorColumn",
 }
 
 ---@param opts Opts | nil
 function Treewalker.setup(opts)
-	if opts then
+  if opts then
     Treewalker.opts = vim.tbl_deep_extend('force', Treewalker.opts, opts)
-	end
+  end
 end
 
 -- TODO This is clever kinda, but it breaks autocomplete of `require('treewalker')`
 
 -- Assign move_{in,out,up,down}
 for fn_name, fn in pairs(movement) do
-	Treewalker[fn_name] = fn
+  Treewalker[fn_name] = fn
 end
 
 -- Assign swap_{up,down}
 for fn_name, fn in pairs(swap) do
-	Treewalker[fn_name] = fn
+  Treewalker[fn_name] = fn
 end
 
 return Treewalker
