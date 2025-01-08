36b6a154329feb7639f72a06113ed6f4de9940a9
Aaron Sullivan <aasullivan1618@gmail.com>
2025-01-07 23:33:17 -0800

    Add rust regression specs
    Woohooo now that we have rust in the tests!

--- tests/treewalker/swap_spec.lua
+++ tests/treewalker/swap_spec.lua
@@ -255,3 +255,27 @@ describe("Swapping in a c file:", function()
     assert.same('        printf("\\ntwo\\n", "one\\n");', lines.get_line(17))
   end)
 end)
+
+describe("Swapping in a rust file:", function()
+  before_each(function()
+    load_fixture("/rust.rs")
+  end)
+
+  it("Moves enum values right", function()
+    vim.fn.cursor(49, 14) -- the o in one
+    assert.same('enum Color { Red, Green, Blue }', lines.get_line(49))
+    tw.swap_right()
+    assert.same('enum Color { Green, Red, Blue }', lines.get_line(49))
+    helpers.assert_cursor_at(49, 21, "Red")
+  end)
+
+  it("Moves enum values left", function()
+    vim.fn.cursor(49, 19) -- the o in one
+    assert.same('enum Color { Red, Green, Blue }', lines.get_line(49))
+    tw.swap_left()
+    assert.same('enum Color { Green, Red, Blue }', lines.get_line(49))
+    helpers.assert_cursor_at(49, 14, "Red")
+  end)
+
+end)
+
