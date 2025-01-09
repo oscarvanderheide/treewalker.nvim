edd581ad18c22d053735d5da540e582ba2fad3cf
Oscar van der Heide <o.vanderheide@umcutrecht.nl>
2025-01-09 19:53:33 +0100

    Trying to add Select to API

--- checks/rogue_util_calls_spec.lua
+++ checks/rogue_util_calls_spec.lua
@@ -2,44 +2,45 @@
 -- util functions like R and log. This isn't perfect, as it tries to exercise
 -- each command, but doesn't seek edge cases.
 
-local load_fixture = require "tests.load_fixture"
-local assert = require 'luassert'
-local stub = require 'luassert.stub'
-local util = require 'treewalker.util'
+local load_fixture = require("tests.load_fixture")
+local assert = require("luassert")
+local stub = require("luassert.stub")
+local util = require("treewalker.util")
 
 local commands = {
-  "Treewalker Up",
-  "Treewalker Down",
-  "Treewalker Right",
-  "Treewalker Left",
-  "Treewalker SwapUp",
-  "Treewalker SwapDown",
-  "Treewalker SwapRight",
-  "Treewalker SwapLeft",
+	"Treewalker Up",
+	"Treewalker Down",
+	"Treewalker Right",
+	"Treewalker Left",
+	"Treewalker Select",
+	"Treewalker SwapUp",
+	"Treewalker SwapDown",
+	"Treewalker SwapRight",
+	"Treewalker SwapLeft",
 }
 
 describe("Extent util calls:", function()
-  local orig_R = util.R
-  local orig_log = util.log
-  local util_R_stub = stub(util, "R")
-  local util_log_stub = stub(util, "log")
-  util_R_stub.callback = orig_R
-  util_log_stub.callback = orig_log
+	local orig_R = util.R
+	local orig_log = util.log
+	local util_R_stub = stub(util, "R")
+	local util_log_stub = stub(util, "log")
+	util_R_stub.callback = orig_R
+	util_log_stub.callback = orig_log
 
-  for _, command in ipairs(commands) do
-    before_each(function()
-      load_fixture("/lua.lua")
-      vim.fn.cursor(31, 26)
-    end)
+	for _, command in ipairs(commands) do
+		before_each(function()
+			load_fixture("/lua.lua")
+			vim.fn.cursor(31, 26)
+		end)
 
-    it(command .. " encounters no " .. "util.R calls", function()
-      vim.cmd(command)
-      assert.stub(util_R_stub).was.called(0)
-    end)
+		it(command .. " encounters no " .. "util.R calls", function()
+			vim.cmd(command)
+			assert.stub(util_R_stub).was.called(0)
+		end)
 
-    it(command .. " encounters no " .. "util.log calls", function()
-      vim.cmd(command)
-      assert.stub(util_log_stub).was.called(0)
-    end)
-  end
+		it(command .. " encounters no " .. "util.log calls", function()
+			vim.cmd(command)
+			assert.stub(util_log_stub).was.called(0)
+		end)
+	end
 end)
--- plugin/init.lua
+++ plugin/init.lua
@@ -1,59 +1,63 @@
 local function tw()
-  -- local util = require "treewalker.util"
-  -- return util.R('treewalker')
-  return require('treewalker')
+	-- local util = require "treewalker.util"
+	-- return util.R('treewalker')
+	return require("treewalker")
 end
 
 local subcommands = {
-  Up = function()
-    tw().move_up()
-  end,
+	Up = function()
+		tw().move_up()
+	end,
 
-  Left = function()
-    tw().move_out()
-  end,
+	Left = function()
+		tw().move_out()
+	end,
 
-  Down = function()
-    tw().move_down()
-  end,
+	Down = function()
+		tw().move_down()
+	end,
 
-  Right = function()
-    tw().move_in()
-  end,
+	Right = function()
+		tw().move_in()
+	end,
 
-  SwapUp = function()
-    tw().swap_up()
-  end,
+	Select = function()
+		tw().select_node()
+	end,
 
-  SwapDown = function()
-    tw().swap_down()
-  end,
+	SwapUp = function()
+		tw().swap_up()
+	end,
 
-  SwapLeft = function()
-    tw().swap_left()
-  end,
+	SwapDown = function()
+		tw().swap_down()
+	end,
 
-  SwapRight = function()
-    tw().swap_right()
-  end
+	SwapLeft = function()
+		tw().swap_left()
+	end,
+
+	SwapRight = function()
+		tw().swap_right()
+	end,
 }
 
 local command_opts = {
-  nargs = 1,
-  complete = function(ArgLead)
-    return vim.tbl_filter(function(cmd)
-      return cmd:match("^" .. ArgLead)
-    end, vim.tbl_keys(subcommands))
-  end
+	nargs = 1,
+	complete = function(ArgLead)
+		return vim.tbl_filter(function(cmd)
+			return cmd:match("^" .. ArgLead)
+		end, vim.tbl_keys(subcommands))
+	end,
 }
 
 local function treewalker(opts)
-  local subcommand = opts.fargs[1]
-  if subcommands[subcommand] then
-    subcommands[subcommand](vim.list_slice(opts.fargs, 2))
-  else
-    print("Unknown subcommand: " .. subcommand)
-  end
+	local subcommand = opts.fargs[1]
+	if subcommands[subcommand] then
+		subcommands[subcommand](vim.list_slice(opts.fargs, 2))
+	else
+		print("Unknown subcommand: " .. subcommand)
+	end
 end
 
 vim.api.nvim_create_user_command("Treewalker", treewalker, command_opts)
