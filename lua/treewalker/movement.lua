6afe2d9459218aeb893128eaeb568f2b8de0d386
Aaron Sullivan <aasullivan1618@gmail.com>
2025-01-11 22:21:41 -0800

    chore: clean movement.lua
    * Fix line spacing in move_out
    * Remove commented out util.log calls

--- lua/treewalker/movement.lua
+++ lua/treewalker/movement.lua
@@ -6,8 +6,8 @@ local M = {}
 ---@return nil
 function M.move_out()
   local target, row, line = targets.out()
+
   if target and row and line then
-    -- util.log("no out candidate")
     operations.jump(row, target)
     return
   end
@@ -18,7 +18,6 @@ function M.move_in()
   local target, row, line = targets.inn()
 
   if target and row and line then
-    --util.log("no in candidate")
     operations.jump(row, target)
   end
 end
@@ -28,7 +27,6 @@ function M.move_up()
   local target, row, line = targets.up()
 
   if target and row and line then
-    --util.log("no up candidate")
     operations.jump(row, target)
   end
 end
@@ -38,7 +36,6 @@ function M.move_down()
   local target, row, line = targets.down()
 
   if target and row and line then
-    --util.log("no down candidate")
     operations.jump(row, target)
   end
 end
